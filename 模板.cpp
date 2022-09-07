4a566abd430adfb5faaf560c273de5e644c73560
star-platinum127 <60998048+star-platinum127@users.noreply.github.com>
2022-09-07 12:22:43 +0800

    Create 模板.cpp

---
+++ 模板.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define mikumywaifu ios_base::sync_with_stdio(0)
+#define mudrockmywaifu cin.tie(0)
+#define pb push_back
+#define ss second
+#define ff first
+#define mem(i,j) memset(i,j,sizeof(i))
+#define pii pair<int,int>
+#define pll pair<long,long>
+#define lowbit(x) x&-x
+const int INF =0x3F3F3F3F;
+const ll LINF=4611686018426387903;
+ll gcd(ll a,ll b){
+    if(b==0) return a;
+    a%=b;
+    return gcd(b,a);
+}
+void debug(){
+    cout<<"DEBUG :";
+    cout<<"\n";
+}
+/*-----------------------------------------*/
+signed main(){
+  return 0;
+}
